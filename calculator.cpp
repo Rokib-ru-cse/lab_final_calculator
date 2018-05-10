a24fbae5b77a373d6e945e8d2a2d3bd01a0a9772
rubel <rubel.h2475@gmail.com>
2018-05-10 10:57:20 +0600

    multiplication function has created

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void multiplication(int a,int b)
+{
+    int mul_value;
+    mul_value = a*b;
+
+    cout << "Multiplicated value: " << mul_value;
+}
+
 int main()
 {
 	int value_1,value_2;
@@ -7,5 +16,7 @@ int main()
 	cin>>value_1;
 	cout<<"Enter any value: ";
 	cin>>value_2;
+
+	multiplication(value_1,value_2);
 	return 0;
 }
