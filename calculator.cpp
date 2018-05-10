3e4d52d0a155e99832322c88e683e4b11937d21b
Manik Hosen <rahathossenmanik@gmail.com>
2018-05-10 10:47:12 +0600

    Modulas Function Created By Manik Hosen

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void Modulas(int number,int divider){
+    int mod;
+    mod=number%divider;
+    cout<<endl<<number<<"%"<<divider<<"="<<mod<<endl;
+    return;
+}
 int main()
 {
 	int value_1,value_2;
@@ -7,5 +14,6 @@ int main()
 	cin>>value_1;
 	cout<<"Enter any value: ";
 	cin>>value_2;
+	Modulas(value_1,value_2);
 	return 0;
 }
