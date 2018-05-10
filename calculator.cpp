40922bc217412c6ade546ec3cebe7d7e6b88a92a
rokib.ru.cse <rokibahmed.ru.cse@gmail.com>
2018-05-10 10:10:25 +0600

    first commit with main function

---
+++ calculator.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int value_1,value_2;
+	cout<<"Enter any value: ";
+	cin>>value_1;
+	cout<<"Enter any value: ";
+	cin>>value_2;
+	return 0;
+}
